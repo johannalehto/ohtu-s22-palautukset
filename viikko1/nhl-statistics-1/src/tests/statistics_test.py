05a5cfd29bb3ff14219cd9c9db0b073ad20437fb
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-11-07 18:08:21 +0200

    Add tests for Statistics

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,51 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+
+    def test_search_by_name(self):
+        player = self.statistics.search("Semenko")
+        self.assertEqual(str(player), "Semenko EDM 4 + 12 = 16")
+
+    def test_search_by_non_existing(self):
+        player = self.statistics.search("Non Existing")
+        self.assertIsNone(player)
+
+    def test_team_filter_players(self):
+        team_name = self.statistics.team("EDM")
+        players_of_team = list(map(lambda player : str(player), team_name))
+        comparison = [
+            "Semenko EDM 4 + 12 = 16",
+            "Kurri EDM 37 + 53 = 90",
+            "Gretzky EDM 35 + 89 = 124"
+        ]
+        self.assertEqual(players_of_team, comparison)
+
+    def test_top_sort_players(self):
+        top_players = self.statistics.top(3)
+        sorted_players = list(map(lambda player: str(player), top_players))
+        comparison = [
+            "Gretzky EDM 35 + 89 = 124",
+            "Lemieux PIT 45 + 54 = 99",
+            "Yzerman DET 42 + 56 = 98"
+        ]
+        self.assertEqual(sorted_players, comparison)
+
+
+    
+
