6a5f1671930b0003498c02a6e57c4df0c1dae1b6
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-11-27 16:22:11 +0200

    Add tests for lataa-method, add conditions to Kassapaate-class methods

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,16 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataa_lisaa_summan_kortille_jos_positiivinen(self):
+        maksukortti_mock = Mock()
+        self.kassa.lataa(maksukortti_mock, 5)
+
+        maksukortti_mock.lataa.assert_called_with(maksukortti_mock, 5)
+
+
+    def test_lataa_ei_lisaa_summaa_kortille_jos_negatiivinen(self):
+        maksukortti_mock = Mock()
+        self.kassa.lataa(maksukortti_mock, -1)
+
+        maksukortti_mock.lataa.assert_not_called() 
