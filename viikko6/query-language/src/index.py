8a5e98f8d0020b83bfad30ef7ed1c84ef00bbc4d
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-12-12 23:09:50 +0200

    Add classes All, Not, HasFewerThan

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,20 +1,28 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
+    # matcher = And(
+    #     Not(HasAtLeast(1, "goals")),
+    #     PlaysIn("NYR")
+    # )
+
     matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(5, "assists"),
-        PlaysIn("PHI")
+        HasFewerThan(1, "goals"),
+        PlaysIn("NYR")
     )
+
 
     for player in stats.matches(matcher):
         print(player)
+
+    filtered_with_all = stats.matches(All())
+    print(f'Total amount of players: {len(filtered_with_all)}')
 
 
 if __name__ == "__main__":
