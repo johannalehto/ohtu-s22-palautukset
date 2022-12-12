915bafecc10c83cf3389aa219e172c2713a7a7cd
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-12-12 23:18:05 +0200

    Add class Or

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
@@ -12,17 +12,30 @@ def main():
     #     PlaysIn("NYR")
     # )
 
-    matcher = And(
-        HasFewerThan(1, "goals"),
-        PlaysIn("NYR")
-    )
+    # matcher = And(
+    #     HasFewerThan(1, "goals"),
+    #     PlaysIn("NYR")
+    # )
 
+    # matcher = Or(
+    #     HasAtLeast(45, "goals"),
+    #     HasAtLeast(70, "assists")
+    # )
+
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
 
     for player in stats.matches(matcher):
         print(player)
 
-    filtered_with_all = stats.matches(All())
-    print(f'Total amount of players: {len(filtered_with_all)}')
+    # filtered_with_all = stats.matches(All())
+    # print(f'Total amount of players: {len(filtered_with_all)}')
 
 
 if __name__ == "__main__":
